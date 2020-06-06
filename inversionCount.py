2368513ab873f0a980fa13b18e9baf3bb1716c8d
mlacosta <marianoacosta.003@gmail.com>
2020-06-06 12:09:20 -0300

    added description

--- inversionCount.py
+++ inversionCount.py
@@ -1,3 +1,12 @@
+# Your task is to compute the number of inversions in the file given, where the 
+# i^{th}ith row of the file indicates the i^{th}ithentry of an array.
+# Because of the large size of this array, you should implement the fast 
+# divide-and-conquer algorithm covered in the video lectures.
+# The numeric answer for the given input file should be typed in the space below.
+# So if your answer is 1198233847, then just type 1198233847 in the space 
+# provided without any space / commas / any other punctuation marks. 
+# You can make up to 5 attempts, and we'll use the best one for grading.
+
 file = open("IntegerArray.txt","r")
 intList = []
 
@@ -27,7 +36,6 @@ def countSplit(listInput):
     while i < lenB and j < lenC:
         if B[i] <= C[j]:
             i+=1
-        
         else:
             counter+= lenB - i
             j+=1
