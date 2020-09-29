035d770056d0939e5ff810aed8859878c7458138
mlacosta <marianoacosta.003@gmail.com>
2020-09-29 12:48:56 -0300

    huffman implemented

--- course_03/problem_03/huffman.py
+++ course_03/problem_03/huffman.py
@@ -5,6 +5,8 @@
 #
 #Huffman encoding with greedy algorithm
 
+#I use a heap for min search and Depth First Search for encoding
+
 ### HEADER ###
 import heapq as hp
 
@@ -23,26 +25,64 @@ class Symbol:
 
     def __lt__(self,other):
         return self.weight < other.weight
-
         
 class Node:
-    def __init__(self, data = None, label = None,left = None ,right = None):
+    def __init__(self, symbol = None, label = None,left = None ,right = None):
         self.label = label
         self.left = left
         self.right = right
-        self.data = data
+        self.symbol = symbol
+        self.explored = False
+    
+    def is_explored(self):
+        return self.explored
+    
+    def __str__(self):
+        return 'Label: %s, Symbol: %s'%(self.label,self.symbol)
 
 def to_symbol(el):
     return Symbol(el[0],el[1])
+
+def create_node(s_tree,label):
+
+    if len(s_tree) == 2:
+        left = Node(symbol = s_tree[0], label = '0')
+        right = Node(symbol = s_tree[1], label = '1')
+        return Node(left=left, right=right, label= '1')
+    
+    else:
+        left = Node(symbol = s_tree[0], label = '0')
+        right = create_node(s_tree[1:],label = '1')
+        return Node(left=left, right=right, label = label)
+    
+    
+def decompress(tree):
+    s_left = tree.left.symbol.split()
+    s_right = tree.right.symbol.split()
+    
+    left = create_node(s_left,'0')
+    right = create_node(s_right, '1')
+    
+    return Node(left=left, right=right)
      
 def huffman(epsilon):
     if len(epsilon) == 2:
-        left = Node(data = epsilon[0], label = '0'), 
-        right = Node(data = epsilon[1], label = '1')
+        left = Node(symbol = epsilon[0].symbol, label = '0')
+        right = Node(symbol = epsilon[1].symbol, label = '1')
         return Node(left=left,right=right)
+    
+    a = hp.heappop(epsilon)
+    b = hp.heappop(epsilon)
+    
+    ab = Symbol(a.symbol + ' ' + b.symbol,a.weight + b.weight)    
+    hp.heappush(epsilon,ab)
+    tree = huffman(epsilon)
+    
+    return tree    
     
 ### IMPLEMENTATION ###
 info, data = read_data('huffman.txt')
 epsilon = list(map(lambda el: Symbol(el[0],el[1]),list(enumerate(data))))
 hp.heapify(epsilon)
-
+tree = huffman(epsilon)    
+encoding  = decompress(tree)
