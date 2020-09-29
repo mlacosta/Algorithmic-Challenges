4b6baa446e18a7a65d0833b546c0e1fbd2906de7
mlacosta <marianoacosta.003@gmail.com>
2020-09-29 16:14:59 -0300

    less code

--- course_03/problem_03/huffman.py
+++ course_03/problem_03/huffman.py
@@ -45,7 +45,7 @@ class Node:
         self.__explored = True
         
     def __str__(self):
-        return 'Label: %s, Symbol: %s Depth: %s'%(self.label,self.symbol,self.depth)
+        return 'Label: %s, Symbol: %s, Depth: %d, Weight: %d'%(self.label,self.symbol,self.depth,self.weight)
 
     def __lt__(self,other):
         return self.weight < other.weight
@@ -56,24 +56,11 @@ class Node:
 def to_symbol(el):
     return Symbol(el[0],el[1])
 
-def create_node(s_tree, label, depth = 0):
-
-    if len(s_tree) == 2:
-        left = Node(symbol = s_tree[0], label = '0',depth = depth + 2)
-        right = Node(symbol = s_tree[1], label = '1',depth = depth + 2)
-        return Node(left=left, right=right, label= '1',depth = depth + 1)
-    
-    else:
-        left = Node(symbol = s_tree[0], label = '0',depth = depth + 2)
-        right = create_node(s_tree[1:],label = '1',depth = depth + 1)
-        return Node(left=left, right=right, label = label, depth = depth + 1)
-
 def merge(a,b):
     a.label = '0'
     b.label = '1'
     
-    ab = Node(symbol = a.symbol + ' ' + b.symbol, 
-              weight = a.weight + b.weight, 
+    ab = Node(weight = a.weight + b.weight, 
               left = a,
               right  = b)
     
@@ -81,23 +68,19 @@ def merge(a,b):
      
 def huffman(epsilon):
     
-    while(len(epsilon) > 2):
+    while(len(epsilon) > 1):
         a = hp.heappop(epsilon)
         b = hp.heappop(epsilon)
         ab = merge(a,b)
         hp.heappush(epsilon,ab)
         
-    a = epsilon[0]
-    b = epsilon[1]
-    return merge(a,b) 
-
-
 def decode(tree,symbol):
     pass
 
   
 ### IMPLEMENTATION ###
 info, data = read_data('huffman.txt')
+data = [3,2,6,8,2,6]
 epsilon = list(map(lambda el: Node(weight = el[1],symbol = str(el[0])),list(enumerate(data))))
 hp.heapify(epsilon)
-tree = huffman(epsilon)    
+huffman(epsilon)    
