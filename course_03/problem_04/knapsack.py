641c913a83f18776963091763d1a3ee455dcbbbe
mlacosta <marianoacosta.003@gmail.com>
2020-10-15 23:15:54 -0300

    course 03 finished

--- course_03/problem_04/knapsack.py
+++ course_03/problem_04/knapsack.py
@@ -42,7 +42,59 @@ def knapsack(capacity ,size, data):
             A[i,x] = A[i-1,x] if (wi > x) else max(A[i-1,x],A[i-1,x - wi] + vi) 
     
     return A[size,capacity-1]
+
+
+
+def knapsackBig(table ,capacity ,size, data, depth = 0):
+    
+    '''
+        Calculates the max value in the knapsack problem
+        
+        params:
+            - capacity (int)
+            - size(int)
+            - data from read_data()
+        
+        returns:
+            - the optimal vax value
+    '''
+    
+    #table [(wn,depth)] = vn 
+    
+    
+    if ((depth,capacity) in table):
+
+        return table[(depth,capacity)]
+    
+    else:
+    
+        if(size == 0) or (capacity ==0):
+            return 0
+        
+        wn = int(list(data[size-1].split())[1])
+        vn = int(list(data[size-1].split())[0])
+                
+        if(wn > capacity):
+            table[(depth,capacity)] = knapsackBig(table ,capacity ,size-1, data[:size-1], depth + 1)
+            
+        
+        else:
+            table[(depth,capacity)] = max( vn + knapsackBig(table ,capacity - wn ,size-1, data[:size-1], depth + 1) ,
+                        knapsackBig(table ,capacity , size-1, data[:size-1], depth + 1)     
+                       )
+        return table[(depth,capacity)] 
+#----------------------------------------------------------------------------------------
         
 (capacity ,size), data = read_data('knapsack1.txt')
-A = knapsack(capacity ,size, data)
+
+print(knapsack(capacity ,size, data))
+print('\n')
+
+
+(capacity ,size), data = read_data('knapsack_big.txt')
+table =  dict()
+
+print(knapsackBig(table ,capacity ,size, data))
+print('\n')
+
 
